61c8668e0796c1ac93b3e98e87e88dbb65dcbac7
Alex Litvinok <litvinok@gmail.com>
2013-01-19 16:45:50 +0300

    added jade & socket.io

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-var path = require('path'),
-    express = require('express'),
-    port = process.env.PORT || 8080,
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
     app = express(),
-    server = require('http').createServer(app);
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+app.configure(function(){
+    app.set('port', process.env.PORT || 8080);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'static')));
+});
 
 
-app.use(express.static(path.join(__dirname, 'static')));
+app.get('/', routes.index);
 
-server.listen(port);
+server.listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
+});

3f9bca14ae5f72e555e0034b665ebb14825ede7d
Mikita Manko <DjComandos@gmail.com>
2013-01-19 21:12:26 +0300

    piano

--- static/scripts/instruments.js
+++ static/scripts/instruments.js
@@ -17,4 +17,17 @@ $(document).ready(function(){
 
     });
 
+    // hack for mobile orientation
+    $(window).bind('orientationchange', function(e, onready){
+    if(onready){
+        $(document.body).addClass('portrait-onready');
+    }
+    if (Math.abs(window.orientation) != 90){
+        $(document.body).addClass('portrait');
+    } 
+    else {
+        $(document.body).removeClass('portrait').removeClass('portrait-onready');
+    }
+    });
+
 });
